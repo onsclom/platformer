064837dacaf2d2739bb3c32eebda5edd195bc84c
Austin Merrick <austinmerrick@onsclom.net>
2025-03-30 16:05:54 -0700

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,33 @@
-// server.ts
 import { Elysia, t } from "elysia";
 import { cors } from "@elysiajs/cors";
+import fs from "fs/promises";
+import { randomUUIDv7 } from "bun";
+
+// if prod use root "/persistent"
+// if dev use local "persistent"
+const PERSISTENT_DIR = process.env.PORT ? "/persistent" : "persistent";
 
 const app = new Elysia()
   .use(cors({ origin: true }))
   .get("/", () => "Hi Elysia")
-  .get("/id/:id", ({ params: { id } }) => id)
-  .post("/mirror", ({ body }) => body, {
-    body: t.Object({
-      id: t.Number(),
-      name: t.String(),
-    }),
+  .get("/levels", async () => {
+    // return all file names in `persistent`
+    return await fs.readdir(PERSISTENT_DIR);
+  })
+  .post(
+    "/level/create",
+    ({ body }) => {
+      // create a new level
+      const uuid = randomUUIDv7();
+      fs.writeFile(`persistent/${uuid}`, body);
+      return uuid;
+    },
+    { body: t.String() },
+  )
+  .get("/level/:level", async ({ params: { level } }) => {
+    return JSON.parse(
+      (await fs.readFile(`${PERSISTENT_DIR}/${level}`)).toString(),
+    );
   })
   .listen({ port: process.env.PORT || 3000 }, () => {
     console.log(`Listening on port ${process.env.PORT || 3000}`);
