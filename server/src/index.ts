26deb1936508f918fb556fbf641b2d08b70b6bfc
Austin Merrick <austinmerrick@onsclom.net>
2025-03-30 16:20:14 -0700

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,14 +5,18 @@ import { randomUUIDv7 } from "bun";
 
 // if prod use root "/persistent"
 // if dev use local "persistent"
-const PERSISTENT_DIR = process.env.PORT ? "/persistent" : "persistent";
+const persistentDir = process.env.PORT ? "/persistent" : "persistent";
+
+const levelsDir = `${persistentDir}/levels`;
+// if levels folder doesn't exist, create it
+await fs.mkdir(levelsDir).catch(() => {});
 
 const app = new Elysia()
   .use(cors({ origin: true }))
   .get("/", () => "Hi Elysia")
   .get("/levels", async () => {
     // return all file names in `persistent`
-    return await fs.readdir(PERSISTENT_DIR);
+    return await fs.readdir(levelsDir);
   })
   .post(
     "/level/create",
@@ -21,15 +25,13 @@ const app = new Elysia()
 
       // create a new level
       const uuid = randomUUIDv7();
-      fs.writeFile(`${PERSISTENT_DIR}/${uuid}`, body);
+      fs.writeFile(`${levelsDir}/${uuid}`, body);
       return uuid;
     },
     { body: t.String() },
   )
   .get("/level/:level", async ({ params: { level } }) => {
-    return JSON.parse(
-      (await fs.readFile(`${PERSISTENT_DIR}/${level}`)).toString(),
-    );
+    return JSON.parse((await fs.readFile(`${levelsDir}/${level}`)).toString());
   })
   .listen({ port: process.env.PORT || 3000 }, () => {
     console.log(`Listening on port ${process.env.PORT || 3000}`);
